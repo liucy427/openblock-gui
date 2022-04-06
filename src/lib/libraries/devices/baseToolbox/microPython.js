238984eaa69ba0b6f00b87ce02694ccb3d99bc30
ArthurZheng <869705086@qq.com>
2022-04-06 19:39:28 +0800

    fix wrong block type

--- src/lib/libraries/devices/baseToolbox/microPython.js
+++ src/lib/libraries/devices/baseToolbox/microPython.js
@@ -367,7 +367,7 @@ const events = function (isInitialSetup, isStage, targetId, isRealtimeMode) {
                 </value>
             </block>
         ` : `
-            <block type="event_whenmicrobitbegin"/>
+            <block type="event_whenmicropythonbegin"/>
         `}
         ${categorySeparator}
     </category>
